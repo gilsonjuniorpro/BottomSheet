500848019d5a61a040e2532cf80ef0dcc24e2aab
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2017-12-14 16:40:09 -0200

    Initial commit

---
+++ app/src/main/java/br/bottomsheet/com/BottomSheetFragment.java
@@ -0,0 +1,29 @@
+package br.bottomsheet.com;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gilsonsantos on 14/12/17.
+ */
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+    public BottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false);
+    }
+}
